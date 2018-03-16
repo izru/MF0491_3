1701c654a0fc553d83c5f562c75d704be91d4a81
Curso <Curso@IPARTEKAULA.COM>
2018-03-16 14:24:45 +0100

    final

--- src/app/providers/mocks.productos.ts
+++ src/app/providers/mocks.productos.ts
@@ -35,6 +35,114 @@ export class MOCKS_PRODUCTOS {
 		"valor": 250,
         "unidad": "gr",
         "precioUnidad":"50 €/kg"    
+    },
+    {
+		"id": 1,
+        "marca": "BEEFEATER",
+        "nombre": "ginebra",
+		"descripcion": "ginebra inglesa botella 70 cl",
+        "precio": 13,
+        "descuento": 20,
+		"foto": "https://www.compraonline.grupoeroski.com/images/20228755.jpg",
+		"valor": 70,
+        "unidad": "cl"  ,
+        "precioUnidad":"18.5 €/l" 
+    },
+    {
+		"id": 2,
+        "marca": "FONTANEDA",
+        "nombre": "galletas",
+		"descripcion": "galletas maria paquete de 250 gr",
+        "precio": 2,
+        "descuento": 0,
+		"foto": "https://www.compraonline.grupoeroski.com/images/13742184.jpg",
+		"valor": 250,
+        "unidad": "gr",
+        "precioUnidad":"3 €/kg"    
+    },
+    {
+		"id": 2,
+        "marca": "NIVEA",
+        "nombre": "crema",
+		"descripcion": "crema de manos de 250 gr",
+        "precio": 5,
+        "descuento": 10,
+		"foto": "https://www.compraonline.grupoeroski.com/images/447128.jpg",
+		"valor": 250,
+        "unidad": "gr",
+        "precioUnidad":"50 €/kg"    
+    },
+    {
+		"id": 1,
+        "marca": "BEEFEATER",
+        "nombre": "ginebra",
+		"descripcion": "ginebra inglesa botella 70 cl",
+        "precio": 13,
+        "descuento": 20,
+		"foto": "https://www.compraonline.grupoeroski.com/images/20228755.jpg",
+		"valor": 70,
+        "unidad": "cl"  ,
+        "precioUnidad":"18.5 €/l" 
+    },
+    {
+		"id": 2,
+        "marca": "FONTANEDA",
+        "nombre": "galletas",
+		"descripcion": "galletas maria paquete de 250 gr",
+        "precio": 2,
+        "descuento": 0,
+		"foto": "https://www.compraonline.grupoeroski.com/images/13742184.jpg",
+		"valor": 250,
+        "unidad": "gr",
+        "precioUnidad":"3 €/kg"    
+    },
+    {
+		"id": 2,
+        "marca": "NIVEA",
+        "nombre": "crema",
+		"descripcion": "crema de manos de 250 gr",
+        "precio": 5,
+        "descuento": 10,
+		"foto": "https://www.compraonline.grupoeroski.com/images/447128.jpg",
+		"valor": 250,
+        "unidad": "gr",
+        "precioUnidad":"50 €/kg"    
+    },
+    {
+		"id": 1,
+        "marca": "BEEFEATER",
+        "nombre": "ginebra",
+		"descripcion": "ginebra inglesa botella 70 cl",
+        "precio": 13,
+        "descuento": 20,
+		"foto": "https://www.compraonline.grupoeroski.com/images/20228755.jpg",
+		"valor": 70,
+        "unidad": "cl"  ,
+        "precioUnidad":"18.5 €/l" 
+    },
+    {
+		"id": 2,
+        "marca": "FONTANEDA",
+        "nombre": "galletas",
+		"descripcion": "galletas maria paquete de 250 gr",
+        "precio": 2,
+        "descuento": 0,
+		"foto": "https://www.compraonline.grupoeroski.com/images/13742184.jpg",
+		"valor": 250,
+        "unidad": "gr",
+        "precioUnidad":"3 €/kg"    
+    },
+    {
+		"id": 2,
+        "marca": "NIVEA",
+        "nombre": "crema",
+		"descripcion": "crema de manos de 250 gr",
+        "precio": 5,
+        "descuento": 10,
+		"foto": "https://www.compraonline.grupoeroski.com/images/447128.jpg",
+		"valor": 250,
+        "unidad": "gr",
+        "precioUnidad":"50 €/kg"    
 	}
 ]`;
 
